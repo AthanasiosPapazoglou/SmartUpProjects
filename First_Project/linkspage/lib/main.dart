19a7bd790cbef91db7ba063a8682fe3eb5b18e46
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-09-20 17:25:38 +0300

    info boxes implemented

--- First_Project/linkspage/lib/main.dart
+++ First_Project/linkspage/lib/main.dart
@@ -31,12 +31,58 @@ class MyApp extends StatelessWidget {
                 Text(
                   'FLUTTER DEVELOPER',
                   style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.8),
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.8),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.blueGrey[900],
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '(+30) 69 555 82 140',
+                        style: TextStyle(
+                            color: Colors.blueGrey[900],
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20),
+                      )
+                    ],
                   ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.blueGrey[900],
+                      ),
+                      SizedBox(
+                      width: 10,
+                     ),
+                     Text(
+                       'athanasiospap95@gmail.com',
+                       style: TextStyle(
+                         color: Colors.blueGrey[900],
+                         fontFamily: 'Source Sans Pro',
+                         fontSize: 20),
+                       ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
