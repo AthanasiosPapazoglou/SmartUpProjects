1ea980fa8ba177528ae09bb2ce6201259a9abc05
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-09-20 15:08:48 +0300

    trial1

--- First_Project/linkspage/lib/main.dart
+++ First_Project/linkspage/lib/main.dart
@@ -1,3 +1,4 @@
+//woot
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -8,21 +9,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.blueGrey[900],
-      body: SafeArea(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
+        body: SafeArea(
           child: Center(
             child: Column(
               children: <Widget>[
-                CircleAvatar( radius: 60,
-                backgroundImage: AssetImage('images/71037997_10206797507092583_8008849504243548160_n (1).jpg'),
+                CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage(
+                      'images/71037997_10206797507092583_8008849504243548160_n (1).jpg'),
                 ),
-                Text('Athanasios Pap', style: TextStyle(fontSize: 40, color: Colors.white, fontWeight: FontWeight.bold ))
+                Text(
+                  'Athanasios Pap',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
               ],
             ),
           ),
         ),
       ),
-  );
- } 
+    );
+  }
 }
